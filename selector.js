b783552a49a158fcd4802962a50843a263492beb
Thales Brederodes Montarroyos Candido <54904851+ThalesBMC@users.noreply.github.com>
2020-05-15 20:48:44 -0300

    Just updating new lessons

--- selector.js
+++ selector.js
@@ -1,4 +1,23 @@
-var button = document.getElementsByTagName("button")[0];
-button.addEventListener("click", function () {
-  alert("click");
-});
+var button = document.getElementById("enter");
+var input = document.getElementById("write");
+var div = document.querySelector("div");
+
+function createEvent() {
+  var teste = document.createElement("p");
+  teste.appendChild(document.createTextNode(input.value));
+  div.appendChild(teste);
+  input.value = "";
+}
+function addEvent() {
+  if (input.value.length > 0) {
+    createEvent();
+  }
+}
+function addEvent2(event) {
+  if (input.value.length > 0 && event.keyCode === 13) {
+    createEvent();
+  }
+}
+button.addEventListener("click", addEvent);
+
+input.addEventListener("keypress", addEvent2);
